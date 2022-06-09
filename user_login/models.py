2a5c79dcac652341772fce20cf7961abcff278f2
Sanskar Mehta <sanskar.inexture@gmail.com>
2022-06-09 19:36:24 +0530

    "Models and other files are edited as per new feature"

--- user_login/models.py
+++ user_login/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.formfields import PhoneNumberField
 
+
 class CustomUser(AbstractUser):
     Is_interviewer = models.BooleanField(default=False)
     Is_first_time = models.BooleanField(default=True)
@@ -10,3 +11,8 @@ class CustomUser(AbstractUser):
 class user_details(models.Model):
     user_phone = PhoneNumberField()
     user_technology = models.CharField(max_length=30)
+    user_12th_marks = models.IntegerField()
+    user_10th_marks = models.IntegerField()
+    user_CPI = models.FloatField()
+    user_CV = models.FileField()
+    accepted_status = models.BooleanField(default=False)
